921f938094b84159acb7477b9dbdfc3b8c705aa4
SergeyKhval <sergeykhval@gmail.com>
2016-03-15 16:43:24 +0300

    refactor: changed stations list into select options

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@ angular.module('pubTran')
   .controller('mainController', function ($scope, Stations) {
     Stations.getAll().then(function(data){
       $scope.stations = data.root.stations.station;
+      $scope.selected = $scope.stations[0];
       $scope.$apply();
     });
   });
