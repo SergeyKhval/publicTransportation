7647311182e9076121172623fd58a40bedf29b61
SergeyKhval <sergeykhval@gmail.com>
2016-03-15 16:32:29 +0300

    refactor: moved BART API key to separate value provider

---
+++ app/scripts/services/bartkey.js
@@ -0,0 +1,2 @@
+angular.module('pubTran')
+  .value('bartKey', 'MW9S-E7SL-26DU-VV8V');
--- app/scripts/services/stations.js
+++ app/scripts/services/stations.js
@@ -1,9 +1,9 @@
 angular.module('pubTran')
-  .factory('Stations', function(x2js){
+  .factory('Stations', function(x2js, bartKey){
     var Stations = {};
 
     Stations.getAll = function(){
-      return fetch('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V').then(function(response){
+      return fetch('http://api.bart.gov/api/stn.aspx?cmd=stns&key=' + bartKey).then(function(response){
         return response.text();
       }).then(function(data){
         return x2js.xml_str2json(data);
