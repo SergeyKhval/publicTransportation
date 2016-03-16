200b603d48e768b1a9ed931b775425073555a008
SergeyKhval <sergeykhval@gmail.com>
2016-03-16 15:56:37 +0300

    feat: implemented basic schedule retrival

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('pubTran')
-  .controller('mainController', function ($scope, Stations) {
+  .controller('mainController', function ($scope, x2js, Stations, Schedule) {
     Stations.getAll().then(function(data){
       $scope.stations = data;
       $scope.selectedDeparture = $scope.stations[0];
       $scope.selectedArrival = $scope.stations[0];
     });
+
+    $scope.getSchedule = function(){
+      Schedule.getSchedule($scope.selectedDeparture.abbr, $scope.selectedArrival.abbr).then(function(data){
+        $scope.schedule = data.root.schedule.request.trip;
+        console.log($scope.schedule);
+      });
+    }
   });
---
+++ app/scripts/services/schedule.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('pubTran')
+  .factory('Schedule', function ($http, x2js, bartKey) {
+    var Schedule = {};
+
+    Schedule.getSchedule = function (departureStation, arrivalStation) {
+      var scheduleUrl = 'http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=' + departureStation + '&dest=' + arrivalStation + '&key=' + bartKey;
+
+      return $http({
+        method: 'GET',
+        url: scheduleUrl
+      }).then(function (response) {
+        return x2js.xml_str2json(response.data);
+      });
+    };
+
+    return Schedule;
+  });
