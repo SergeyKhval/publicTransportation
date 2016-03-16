843493f6b61ce01a3b2bb159ed82d534ed3dd5eb
SergeyKhval <sergeykhval@gmail.com>
2016-03-16 12:38:13 +0300

    feat: added station selection form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,8 @@ angular.module('pubTran')
   .controller('mainController', function ($scope, Stations) {
     Stations.getAll().then(function(data){
       $scope.stations = data.root.stations.station;
-      $scope.selected = $scope.stations[0];
+      $scope.selectedDeparture = $scope.stations[0];
+      $scope.selectedArrival = $scope.stations[0];
       $scope.$apply();
     });
   });
