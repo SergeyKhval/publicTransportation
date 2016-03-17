c649c46f1b6bdf7bda1cea81058e7560bf0b44b0
SergeyKhval <sergeykhval@gmail.com>
2016-03-17 11:33:20 +0300

    refactor: moved service worker instantiation to angular run block

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('pubTran')
-  .config(['$routeProvider', function($routeProvider){
+  .config(['$routeProvider', 'SwProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'mainController'
       })
-  }]);
+  }])
+  .run(['Sw', angular.noop]);
