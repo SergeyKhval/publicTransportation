41530a420c5e3ad46bf9666b089b8b2bcc15aed1
Sergey Khval <sergeykhval@gmail.com>
2016-04-25 23:01:53 +0300

    feat: add filter routes for proper schedule

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,57 @@
 angular.module('pubTran')
   .controller('mainController', ['$scope', 'Stations', 'Schedule', 'Routes', function ($scope, Stations, Schedule, Routes) {
 
+    function filterByFirstStation(routes, station) {
+      let result = [];
+
+      for (let route in routes) {
+        let stops = routes[route].filter(train => {
+          return train.stop.filter(stop => {
+              return stop._station === station;
+            }).length > 0
+        });
+
+        if (stops.length) {
+          result.push(stops);
+        }
+      }
+
+      return result;
+    }
+
+    function filterBySecondStation(routes, firstStation, secondStation) {
+      let filteredByFirstStation = filterByFirstStation(routes, firstStation),
+        resultTrains = [];
+
+      filteredByFirstStation.forEach(trains => {
+        trains.forEach(train => {
+          let seenFirstStation = false,
+            seenFirstBeforeSecond = false;
+
+          for (let i = 0; i < train.stop.length; i++) {
+            if (train.stop[i]._station === firstStation) seenFirstStation = true;
+            if (train.stop[i]._station === secondStation && seenFirstStation) seenFirstBeforeSecond = true;
+          }
+
+          if (seenFirstBeforeSecond) resultTrains.push(train);
+        });
+      });
+
+      return resultTrains;
+    }
+
+    function filterUnneededStation(trains, departureStation, arrivalStation) {
+      let result = [];
+
+      trains.forEach(train => {
+        result.push(train.stop.filter(stop => {
+          return (stop._station === departureStation || stop._station === arrivalStation);
+        }))
+      });
+
+      return result;
+    }
+
     $scope.stationSchedule = [];
 
     $scope.loadingForm = true;
@@ -45,8 +96,13 @@ angular.module('pubTran')
     };
 
     Routes.getAll().then(() => {
-      $scope.routes = Routes.routes;
-      console.log($scope.routes);
+
+      let filteredRoutes = filterByFirstStation(Routes.routes, 'PITT'),
+        filteredBySecond = filterBySecondStation(filteredRoutes, 'PITT', '12TH'),
+        schedule = filterUnneededStation(filteredBySecond, 'PITT', '12TH');
+
+      console.log(schedule);
     });
+
 
   }]);
