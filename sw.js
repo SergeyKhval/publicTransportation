c35063d123f9c9b4e1d8d1ba4635d1828aec711f
Sergey Khval <sergeykhval@gmail.com>
2016-05-04 23:43:40 +0300

    Update 2016-05-04T20:43:29.887Z

--- sw.js
+++ sw.js
@@ -1,10 +1,45 @@
 var resourcesToCache = [
+  '/',
+  'views/main.html',
+  'templates/pagination.html',
+
   'styles/main.css',
+
   'scripts/vendor.js',
-  'scripts/main.js'
+  'scripts/main.js',
+  'scripts/routes.js',
+  'scripts/config/config.js',
+  'scripts/services/idb.js',
+  'scripts/services/sw.js',
+  'scripts/services/stations.js',
+  'scripts/services/trains.js',
+  'scripts/services/realTimeSchedule.js',
+  'scripts/controllers/main.js',
+
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/angular/angular.js',
+  'bower_components/angular-animate/angular-animate.js',
+  'bower_components/angular-route/angular-route.js',
+  'bower_components/angular-touch/angular-touch.js',
+  'bower_components/tether/dist/js/tether.js',
+  'bower_components/bootstrap/dist/js/bootstrap.js',
+  'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+  'bower_components/x2js/xml2json.min.js',
+  'bower_components/angular-x2js/dist/x2js.min.js',
+  'bower_components/bootstrap-material-design/scripts/material.js',
+  'bower_components/bootstrap-material-design/scripts/ripples.js',
+  'bower_components/arrive/src/arrive.js',
+  'bower_components/indexeddb-promised/lib/idb.js',
+
+  'bower_components/font-awesome/fonts/fontawesome-webfont.woff?v=4.6.1',
+  'bower_components/font-awesome/fonts/fontawesome-webfont.woff2?v=4.6.1',
+  'bower_components/font-awesome/fonts/fontawesome-webfont.eot?v=4.6.1',
+  'bower_components/font-awesome/fonts/fontawesome-webfont.svg?v=4.6.1',
+  'bower_components/font-awesome/fonts/fontawesome-webfont.ttf?v=4.6.1',
+  'bower_components/font-awesome/fonts/FontAwesome.otf?v=4.5.0'
 ];
 
-var staticCacheName = 'pubTran-static-v1';
+var staticCacheName = 'pubTran-static-v2';
 
 var allCaches = [
   staticCacheName
@@ -18,14 +53,13 @@ self.addEventListener('install', function (event) {
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('pubTran-') &&
-            !allCaches.includes(cacheName);
-        }).map(function(cacheName) {
+        cacheNames.filter(function (cacheName) {
+          return cacheName.startsWith('pubTran-') && !allCaches.includes(cacheName);
+        }).map(function (cacheName) {
           return caches.delete(cacheName);
         })
       );
@@ -33,9 +67,9 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event){
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
